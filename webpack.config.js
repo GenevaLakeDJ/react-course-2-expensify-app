73d318a8312aad4a7f770ee1be4fd97259bec02e
Charles Clow <charlesclow@gmail.com>
2019-05-09 11:56:54 -0500

    Localhost 8080 won't load

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -43,9 +43,10 @@ module.exports = (env) => {
         // this devtool shows us exactly where errors, etc. exist in the dev tools (versus showing line 22000 in the compiled bundle.js file)
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public'),
+            contentBase: path.join(__dirname, 'public', ),
             // the below tells the dev-server that we are handling routing via client side code, so it should return "this page" (index?) for all 404 routes
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: './dist/'
         }
     }
 }
