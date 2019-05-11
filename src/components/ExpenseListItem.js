bfe18a678c5340db412ae826f9015a9d29f9092e
Charles Clow <charlesclow@gmail.com>
2019-05-11 08:58:19 -0500

    Set up formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,6 +3,8 @@
 // need to import react because we are using jsx
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 // Need to add dispatch since we destructured the props item.  Adding dispatch here is the same as calling props.dispatch, but props is undefined right now since we destructured the object
 // feed id as an object into removeExpense, as that is how the function is set up
@@ -11,7 +13,11 @@ const ExpenseListItem = ( {dispatch, id, description, amount, createdAt} ) => (
         <Link to={`/edit/${id}`}>
             <h3> {description} </h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount/100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
 
